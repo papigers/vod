62ccc2a0e9cb7e55572494cdaa2cce676cb279e1
sagi levy <slevy35@gmail.com>
2018-09-20 12:20:47 +0300

    componnet created

--- packages/vod-web/client/src/components/VideoComment/VideoComment.js
+++ packages/vod-web/client/src/components/VideoComment/VideoComment.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { Flex, Box } from 'grid-styled';
 import { Link } from 'react-router-dom';
 
 import { Persona, PersonaSize } from 'office-ui-fabric-react/lib/Persona';
 import { Label } from 'office-ui-fabric-react/lib/Label';
+
+
+const CommentText = styled.p`
+  margin: 5px;
+`;
+
+const CommentSection = styled.div`
+  margin-bottom: 15px;
+`;
 
 class VideoComment extends Component {
   // onRenderPersona = ()  =>  {
@@ -21,6 +31,7 @@ class VideoComment extends Component {
       // } = this.props;
 
         return(
+          <CommentSection>
             <Flex>
                 <Box width={200}>
                   <Link to={`/channel/s7591665`}>
@@ -32,8 +43,11 @@ class VideoComment extends Component {
                     />
                   </Link>
                 </Box>
-                <p>סרטון מטורףףף</p>
+                <Box>
+                <CommentText>סרטון מטורףףף</CommentText>
+                </Box>
             </Flex>
+          </CommentSection>
         );
     }
 }
