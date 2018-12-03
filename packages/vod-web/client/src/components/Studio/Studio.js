861240bac7e986b64c5a853378b5723b4887814c
sagi levy <slevy35@gmail.com>
2018-12-03 17:58:45 +0200

    ui fixed

--- packages/vod-web/client/src/components/Studio/Studio.js
+++ packages/vod-web/client/src/components/Studio/Studio.js
@@ -1,14 +1,15 @@
 import React, { Component, Fragment } from 'react';
-import { Box } from 'grid-styled';
-import { transitions } from 'polished';
+import { Box, Flex } from 'grid-styled';
 import styled from 'styled-components';
 
 import { Pivot, PivotItem, PivotLinkSize } from 'office-ui-fabric-react/lib/Pivot';
 import { Selection } from 'office-ui-fabric-react/lib/Selection';
 import { MarqueeSelection } from 'office-ui-fabric-react/lib/MarqueeSelection';
-import { OverflowSet } from 'office-ui-fabric-react/lib/OverflowSet';
-import { CommandBarButton } from 'office-ui-fabric-react/lib/Button';
-import { ShimmeredDetailsList } from 'office-ui-fabric-react/lib/ShimmeredDetailsList';
+import { Sticky, StickyPositionType } from 'office-ui-fabric-react/lib/Sticky';
+import { ScrollablePane, ScrollbarVisibility } from 'office-ui-fabric-react/lib/ScrollablePane';
+import { DetailsList, SelectionMode } from 'office-ui-fabric-react/lib/DetailsList';
+import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
+import { TooltipHost } from 'office-ui-fabric-react/lib/Tooltip';
 
 import { videosColumns } from './Columns';
 import VideoThumbnail from 'components/VideoThumbnail';
@@ -17,17 +18,25 @@ import EditMetadata from './EditMetadata';
 import DeleteForm from './DeleteForm';
 import EditPrivacy from './EditPrivacy';
 
-const ContentBox = styled(Box).attrs({
-    pr: 30,
-    pl: 30,
+
+const ContentBox = styled(Flex).attrs({
+    position: 'relative',
+    height: '100%',
   })``;
 
 const ActionsBox = styled(Box).attrs({
-    pt: 15,
-    pb: 15,
-  })``;
+    // pt: 15,
+    // pb: 15,
+  })`
+  max-height: ${({ hasItems }) => hasItems ? 40 : 0}px;
+  overflow: hidden;
+  transition: max-height 200ms ease-in-out;
+`;
 
-const TitleBox = ContentBox.extend`
+const TitleBox = styled(Box).attrs({
+    pr: 30,
+    pl: 30,
+  })`
   .ms-Shimmer-shimmerWrapper {
     background: ${({theme}) => `linear-gradient(to left, ${theme.palette.neutralLight} 0%, ${theme.palette.neutralQuaternaryAlt} 50%, ${theme.palette.neutralLight} 100%) 0px 0px / 90% 100% no-repeat ${theme.palette.neutralQuaternaryAlt}`};
   }
@@ -48,6 +57,14 @@ const CategoryHeader = styled.h2`
   display: inline-block;
 `;
 
+const StudioContainer = styled(Flex)`
+  /* width: ${({sidebarisopen}) => sidebarisopen ? 'calc(100vw - 240px)' : '100vw'}; */
+  height: calc(100vh - 64px);
+  transition: width 300ms ease-in-out;
+  box-sizing: border-box;
+  overflow: auto;
+`;
+
 class Studio extends Component {
     constructor() {
         super();
@@ -59,7 +76,7 @@ class Studio extends Component {
             selection: new Selection({ onSelectionChanged: this.onSelectionChanged }),
             selectionDetails : [],
             modalIsOpen: false,
-            editType: ''
+            editType: '',
         };
       }
 
@@ -89,7 +106,7 @@ class Studio extends Component {
                         likesCount: video.likesCount,
                         commentsCount: video.commentsCount,
                         createdAt: new Date(video.createdAt).toLocaleDateString('hebrew',options),
-                });
+                    });
                 });
                 groups.push({
                     key: key,
@@ -141,7 +158,17 @@ class Studio extends Component {
         });
       }
 
-    renderModal(){
+    onRenderDetailsHeader(props, defaultRender) {
+        return (
+        <Sticky stickyPosition={StickyPositionType.Header} isScrollSynced={true}>
+            {defaultRender({
+                ...props,
+                onRenderColumnHeaderTooltip: (tooltipHostProps) => <TooltipHost {...tooltipHostProps} />
+            })}
+        </Sticky>);
+    }
+
+    renderModal() {
           const {editType, selectionDetails} = this.state;
           const {onMetadataEdit, onVideoShare, onDelete, onTagsEdit} = this.props;
           switch (editType) {
@@ -171,94 +198,148 @@ class Studio extends Component {
       }
 
     renderTab() {
-        const {groups, items, activeTab , selection, selectionDetails,isDataLoaded} = this.state;
+        const {
+            groups,
+            items,
+            activeTab,
+            selection,
+            selectionDetails,
+            isDataLoaded,
+        } = this.state;
 
         switch (activeTab) {
           case 'videos':
             selection.setItems(this.state.items, false)
             return (
-                <Box>
-                    {selectionDetails.length ? 
-                        <ActionsBox>
-                        <OverflowSet
-                            className="ms-fadeIn500"
-                            items={[
-                                {
-                                    key: 'edit',
-                                    name: 'ערוך',
-                                    icon: 'Edit',
-                                    subMenuProps: {
-                                        items: [
-                                        {
-                                            key: 'name',
-                                            name: 'שם',
-                                            onClick: () => {
-                                                this.onMenuClick('name');
-                                              }
-                                        },{
-                                            key: 'description',
-                                            name: 'תיאור',
-                                            onClick: () => {
-                                                this.onMenuClick('description');
-                                              }
-                                        },{
-                                            key: 'tags',
-                                            name: 'תגיות',
-                                            onClick: () => {
-                                                this.onMenuClick('tags');
-                                              }
-                                        },{
-                                            key: 'thumbnail',
-                                            name: 'תמונה',
-                                            onClick: () => {
-                                                this.onMenuClick('thumbnail');
-                                              }
+                <Fragment>
+                    <ActionsBox hasItems={selectionDetails.length}>
+                            <CommandBar
+                                items={selectionDetails.length <= 1 ? [
+                                    {
+                                        key: 'editVideo',
+                                        name: 'ערוך סרטון',
+                                        iconProps: {
+                                            iconName: 'Edit',
+                                        },
+                                        onClick: () => {
+                                            this.onMenuClick('editVideo');
                                         }
-                                        ]
-                                      }
-                                },{
-                                    key: 'share',
-                                    name: 'שתף',
-                                    icon: 'Share',
-                                    onClick: () => {
-                                        this.onMenuClick('share');
-                                      }
-                                },{
-                                    key: 'delete',
-                                    name: 'מחק',
-                                    icon: 'Delete',
-                                    onClick: () => {
-                                        this.onMenuClick('delete');
-                                      }
-                                }
-                            ]}
-                            onRenderItem={(item) => {
-                                if (item.onRender) {
-                                    return item.onRender(item);
-                                }
-                                if (selectionDetails.length < 2 || item.key !== 'share') {
-                                    return <CommandBarButton split={true} style={{ height: '35px' }} iconProps={{ iconName: item.icon }} menuProps={item.subMenuProps} text={item.name} onClick={item.onClick}/>;
-                                }
-                            }}
-                        /></ActionsBox> : null}
-                    <MarqueeSelection selection={selection}>
-                        <ShimmeredDetailsList
-                            setKey="items"
-                            items={items}
-                            groups={groups}
-                            columns={videosColumns}
-                            selection={selection}
-                            selectionPreservedOnEmptyClick={true}
-                            ariaLabelForSelectAllCheckbox="Toggle selection for all items"
-                            ariaLabelForSelectionColumn="Toggle selection"
-                            groupProps={{
-                                showEmptyGroups: true
-                            }}
-                            enableShimmer={!isDataLoaded}
-                            listProps={{ renderedWindowsAhead: 0, renderedWindowsBehind: 0 }}
-                        />
-                    </MarqueeSelection>
-                </Box>
+                                    }, {
+                                            key: 'delete',
+                                            name: 'מחק',
+                                            iconProps: {
+                                                iconName: 'Delete',
+                                            },
+                                            onClick: () => {
+                                                this.onMenuClick('delete');
+                                            }
+                                        }
+                                    ] : [
+                                        {
+                                            key: 'edit',
+                                            name: 'ערוך',
+                                            iconProps: {
+                                                iconName: 'Edit',
+                                            },
+                                            subMenuProps: {
+                                                items: [
+                                                {
+                                                    key: 'name',
+                                                    name: 'שם',
+                                                    iconProps: {
+                                                        iconName: 'Rename',
+                                                    },
+                                                    onClick: () => {
+                                                        this.onMenuClick('name');
+                                                    }
+                                                },{
+                                                    key: 'description',
+                                                    name: 'תיאור',
+                                                    iconProps: {
+                                                        iconName: 'AlignRight',
+                                                    },
+                                                    onClick: () => {
+                                                        this.onMenuClick('description');
+                                                    }
+                                                },{
+                                                    key: 'tags',
+                                                    name: 'תגיות',
+                                                    iconProps: {
+                                                        iconName: 'tag',
+                                                    },
+                                                    onClick: () => {
+                                                        this.onMenuClick('tags');
+                                                    }
+                                                },{
+                                                    key: 'thumbnail',
+                                                    name: 'תמונה',
+                                                    iconProps: {
+                                                        iconName: 'PictureCenter',
+                                                    },
+                                                    onClick: () => {
+                                                        this.onMenuClick('thumbnail');
+                                                    }
+                                                }
+                                                ]
+                                            }
+                                        },{
+                                            key: 'share',
+                                            name: 'שתף',
+                                            iconProps: {
+                                                iconName: 'Share',
+                                            },
+                                            onClick: () => {
+                                                this.onMenuClick('share');
+                                            }
+                                        },{
+                                            key: 'delete',
+                                            name: 'מחק',
+                                            iconProps: {
+                                                iconName: 'Delete',
+                                            },
+                                            onClick: () => {
+                                                this.onMenuClick('delete');
+                                            }
+                                        }
+                                    ]}
+                                    farItems={[
+                                        {
+                                            key: 'info',
+                                            name: selectionDetails.length === 1 ?
+                                                `נבחר פריט אחד`: `נחברו ${selectionDetails.length} פריטים`,
+                                            iconProps: {
+                                                iconName: 'Info',
+                                            }
+                                        }
+                                    ]}
+                            />
+                        </ActionsBox>
+                <div style={{position: 'relative', flexGrow: 1, flexShrink: 0}}>
+                <ScrollablePane scrollbarVisibility={ScrollbarVisibility.always}>
+                    <Box>
+                        <MarqueeSelection selection={selection}>
+                            <DetailsList
+                                setKey="items"
+                                items={items}
+                                groups={groups}
+                                columns={videosColumns}
+                                selection={selection}
+                                selectionPreservedOnEmptyClick={true}
+                                ariaLabelForSelectAllCheckbox="Toggle selection for all items"
+                                ariaLabelForSelectionColumn="Toggle selection"
+                                groupProps={{
+                                    showEmptyGroups: true
+                                }}
+                                selectionMode={SelectionMode.multiple}
+                                onRenderDetailsHeader={this.onRenderDetailsHeader}
+                                listProps={{ renderedWindowsAhead: 1, renderedWindowsBehind: 1 }}
+                            />
+                        </MarqueeSelection>
+                    </Box>
+                    </ScrollablePane>
+                </div>
+                </Fragment>
+
             );
           case 'playlists':
             return (
@@ -279,26 +360,27 @@ class Studio extends Component {
         const {modalIsOpen} = this.state;
         return (
             <Fragment>
-                <TitleBox>
-                    <CategoryHeader>{'סטודיו'}</CategoryHeader>
-                    <Pivot linkSize={PivotLinkSize.large} headersOnly onLinkClick={this.onLinkClick}>
-                        <PivotItem itemIcon="MSNVideos" linkText="סרטונים" itemKey='videos' />
-                        <PivotItem itemIcon='Stack' linkText="פלייליסטים" itemKey='playlists' />
-                        <PivotItem itemIcon = 'BIDashboard' linkText="אנליטיקות" itemKey="analytics" />
-                    </Pivot>
-                </TitleBox>
-                <ContentBox>
-                    {this.renderTab()}
-                </ContentBox>
+                    <StudioContainer flexDirection="column">
+                        <Flex flexDirection="column" style={{ position: 'relative', height: '100%' }}>
+                            <TitleBox>
+                                <CategoryHeader>{'סטודיו'}</CategoryHeader>
+                                <Pivot linkSize={PivotLinkSize.large} headersOnly onLinkClick={this.onLinkClick}>
+                                    <PivotItem itemIcon="MSNVideos" linkText="סרטונים" itemKey='videos' />
+                                    <PivotItem itemIcon='Stack' linkText="פלייליסטים" itemKey='playlists' />
+                                    <PivotItem itemIcon = 'BIDashboard' linkText="אנליטיקות" itemKey="analytics" />
+                                </Pivot>
+                            </TitleBox>
+                            {this.renderTab()}
+                        </Flex>
+                    </StudioContainer>
                 <Modal
-                isOpen= {modalIsOpen}
-                title= "עריכת סרטון"
-                onDismiss= {this.changeModalState}
-                >
+                    isOpen= {modalIsOpen}
+                    title= "עריכת סרטון"
+                    onDismiss= {this.changeModalState}
+                    >
                     {this.renderModal()}
                 </Modal>
             </Fragment>
-
         );
     }
 }
