83b3e8db6b7cc0f06f51f8707142e04e0bbac594
Gershon Papi <papigers93@gmail.com>
2018-12-23 01:34:05 +0200

    fix linting issue

--- packages/vod-client/src/components/VideoStateDropdown/VideoStateDropdown.js
+++ packages/vod-client/src/components/VideoStateDropdown/VideoStateDropdown.js
@@ -11,8 +11,6 @@ const DropdownOption = styled.div`
   justify-content: center;
   height: 100%;
 
-  ${props => console.log(props)}
-
   .ms-Dropdown-item:hover &,
   .ms-Dropdown-item:hover & .ms-Persona-primaryText {
     color: ${({ theme }) => theme.palette.themePrimary};
