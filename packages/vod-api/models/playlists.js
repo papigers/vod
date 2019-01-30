7d62e6563b233845c9e87a42298a83b81a285877
sagi levy <slevy35@gmail.com>
2019-01-30 15:54:47 +0200

    module done

--- packages/vod-api/models/playlists.js
+++ packages/vod-api/models/playlists.js
@@ -32,8 +32,8 @@ module.exports = function(db) {
     },
     state: {
       type: 'enu',
-      default: 'PUBLIC',
-      values: ['PRIVATE', 'PUBLIC', 'UNLISTED'],
+      default: 'PUBLISHED',
+      values: ['PUBLISHED', 'UNLISTED', 'DRAFT'],
       notNullable: true,
     },
     channelId: {
@@ -50,16 +50,18 @@ module.exports = function(db) {
   playlists.createdAt = true;
   playlists.updatedAt = true;
 
-  playlists.getPlaylists = function(user) {
-    return db.knexnest(db.knex
+  playlists.getManagedPlaylists = function(user) {
+    return db.knexnest( db.knex
       .select(
         `${playlists.table}.id as _id`,
         `${playlists.table}.name as _name`,
         `${playlists.table}.description as _description`,
         `${playlists.table}.state as _state`,
-        `${db.videos.table}.id as _videos__id`,
-        `${db.videos.table}.name as _videos__name`,
-        `${db.videos.table}.description as _videos__description`,
+        `playlistVideo.id as _videos__id`,
+        `playlistVideo.name as _videos__name`,
+        `playlistVideo.description as _videos__description`,
+        `playlistVideo.state as _videos__state`,
+        `videoChannel.name as _videos__channelName`,
         `${db.playlistVideos.table}.position as _videos__position`,
         `${db.channels.table}.id as _channel_id`,
         `${db.channels.table}.name as _channel_name`,
@@ -69,9 +71,10 @@ module.exports = function(db) {
       )
       .from(playlists.table)
       .leftJoin(db.playlistVideos.table, `${playlists.table}.id`, `${db.playlistVideos.table}.playlistId`)
-      .leftJoin(db.channels.table, `${playlists.table}.channelId`, `${db.channels.table}.id`)
-      .leftJoin(db.videos.table, `${playlists.table}.videoId`, `${db.videos.table}.id`)
-      .orderBy(`${playlists.table}.personal`, 'desc')
+      .leftJoin(`${db.channels.table}`, `${playlists.table}.channelId`, `${db.channels.table}.id`)
+      .leftJoin(`${db.videos.table} as playlistVideo`, `${db.playlistVideos.table}.videoId`, `playlistVideo.id`)
+      .leftJoin(`${db.channels.table} as videoChannel`, `playlistVideo.channelId`, `videoChannel.id`)
+      .orderBy(`${db.channels.table}.personal`, 'desc')
       .orderBy(`${playlists.table}.name`, 'desc')
       .orderBy(`${playlists.table}.createdAt`, 'desc')
       .orderBy('_videos__position')
@@ -86,9 +89,11 @@ module.exports = function(db) {
         `${playlists.table}.name as _name`,
         `${playlists.table}.description as _description`,
         `${playlists.table}.state as _state`,
-        `${db.videos.table}.id as _videos__id`,
-        `${db.videos.table}.name as _videos__name`,
-        `${db.videos.table}.description as _videos__description`,
+        `playlistVideo.id as _videos__id`,
+        `playlistVideo.name as _videos__name`,
+        `playlistVideo.description as _videos__description`,
+        `playlistVideo.state as _videos__state`,
+        `videoChannel.name as _videos__channelName`,
         `${db.playlistVideos.table}.position as _videos__position`,
         `${db.channels.table}.id as _channel_id`,
         `${db.channels.table}.name as _channel_name`,
@@ -99,9 +104,10 @@ module.exports = function(db) {
       .from(playlists.table)
       .where(`${playlists.table}.id`, id)
       .leftJoin(db.playlistVideos.table, `${playlists.table}.id`, `${db.playlistVideos.table}.playlistId`)
-      .leftJoin(db.channels.table, `${playlists.table}.channelId`, `${db.channels.table}.id`)
-      .leftJoin(db.videos.table, `${playlists.table}.videoId`, `${db.videos.table}.id`)
-      .orderBy(`${playlists.table}.personal`, 'desc')
+      .leftJoin(`${db.channels.table}`, `${playlists.table}.channelId`, `${db.channels.table}.id`)
+      .leftJoin(`${db.videos.table} as playlistVideo`, `${db.playlistVideos.table}.videoId`, `playlistVideo.id`)
+      .leftJoin(`${db.channels.table} as videoChannel`, `playlistVideo.channelId`, `videoChannel.id`)
+      .orderBy(`${db.channels.table}.personal`, 'desc')
       .orderBy(`${playlists.table}.name`, 'desc')
       .orderBy(`${playlists.table}.createdAt`, 'desc')
       .orderBy('_videos__position')
@@ -112,77 +118,150 @@ module.exports = function(db) {
   playlists.createPlaylist = function(user, playlist) {
     var playlistId = generateId();
     return db.knex.transaction(function(trx) {
-      return trx(playlists.table)
-        .insert({
-          id: playlistId,
-          name: playlist.name,
-          description: playlist.description,
-          state: playlist.state,
-          channelId: playlist.channelId,
-        })
-        .modify(channels.authorizedManageSubquery, user, playlists)
-        .then((inserts) => {
-          if (inserts && inserts.length) {
-            return trx(playlistVideos.table)
-            .where('playlistId', inserts[0].id)
-            .modify(db.channels.authorizedManageSubquery, user)
-            .del()
-            .then(() => {
-              var videos = playlist.videos || [];
-              var position;
-              return videos.map(video => {
-                position = videos.indexOf(video);
-                return trx(playlistVideos.table)
-                .insert({
-                  playlistId: playlistId,
-                  videoId: video.id,
-                  position: position,
-                })
-              });
-            });
-          }
-        })
-        .catch(function(err) {
-          return trx.rollback(new PlaylistError(err, 500));
-        });
+      return trx.select()
+      .from(db.channels.table)
+      .where(`${db.channels.table}.id`, playlist.channelId)
+      .modify(db.channels.authorizedViewSubquery, user)
+      .then((rows) => {
+        if (rows.length > 0) {
+          return trx(playlists.table)
+          .insert({
+            id: playlistId,
+            name: playlist.name,
+            description: playlist.description,
+            state: playlist.state,
+            channelId: playlist.channelId,
+          })
+          .then((inserts) => {
+            if (inserts) {
+                var videos = playlist.videos || [];
+                return Promise.all( 
+                  videos.map(video => {
+                    var position = videos.indexOf(video);
+                    return trx(db.playlistVideos.table)
+                    .insert({
+                      playlistId: playlistId,
+                      videoId: video.id,
+                      position: position,
+                    })
+                  })
+                );
+            }
+          });
+        }
+      })
+      .catch(function(err) {
+        return trx.rollback(new PlaylistError(err, 500));
       });
+    });
   };
 
   playlists.updatePlaylist = function(user, id, playlist) {
     return db.knex.transaction(function(trx) {
-      return trx(playlists.table)
-        .update({
-          name: playlist.name,
-          description: playlist.description,
-          state: playlist.state,
-        })
-        .where('id', id)
-        .modify(channels.authorizedManageSubquery, user, playlists)
-        .then((updatedRows) => {
-          if (updatedRows && updatedRows.length) {
-            return trx(playlistVideos.table)
-            .where('playlistId', updatedRows[0].id)
-            .modify(db.channels.authorizedManageSubquery, user)
-            .del()
-            .then(() => {
-              var videos = playlist.videos || [];
-              var position;
-              return videos.map(video => {
-                position = videos.indexOf(video);
-                return trx(playlistVideos.table)
-                .insert({
-                  playlistId: playlistId,
-                  videoId: video.id,
-                  position: position,
-                })
-              });
-            });
-          }
-        })
-        .catch(function(err) {
-          return trx.rollback(new PlaylistError(err.message, err.code));
+      return trx(`${playlists.table}`)
+      .where('id', id)
+      .andWhere(function() {
+        this.whereIn(`${playlists.table}.channelId`, function() {
+          this.select(`${db.channels.table}.id`)
+            .from(db.channels.table)
+            .modify(db.channels.authorizedManageSubquery, user);
         });
+      })
+      .update({
+        name: playlist.name,
+        description: playlist.description,
+        state: playlist.state,
+      })
+      .then((updatedRows) => {
+        if (updatedRows) {
+          return trx(db.playlistVideos.table)
+          .where('playlistId', id)
+          .del()
+          .then(() => {
+            var videos = playlist.videos || [];
+            var position = 0;
+            return Promise.all(
+              videos.map(video => {
+                return trx(db.playlistVideos.table)
+                .insert({
+                  playlistId: id,
+                  videoId: video.id,
+                  position: position++,
+                });
+              })
+            );
+          });
+        }
+      })
+      .catch(function(err) {
+        return trx.rollback(new PlaylistError(err.message, err.code));
       });
+    });
+  };
+
+  playlists.addVideoToPlaylist = function(user, id, videoId) {
+    return db.knexnest( db.knex
+      .select(
+        `${playlists.table}.id as _id`,
+        `${db.playlistVideos.table}.videoId as _videos__videoId`,
+        `${db.playlistVideos.table}.position as _videos__position`,
+        `${playlists.table}.channelId as _channelId`,
+      )
+      .from(playlists.table)
+      .where(`${playlists.table}.id`, id)
+      .leftJoin(db.playlistVideos.table, `${playlists.table}.id`, `${db.playlistVideos.table}.playlistId`)
+      .leftJoin(db.channels.table, `${db.channels.table}.id`, `${playlists.table}.channelId`)
+      .modify(db.channels.authorizedManageSubquery, user)
+    )
+    .then(function(rows) {
+      if (rows.length) {
+        var playlist = rows[0];
+        if (playlist.videos.filter(video => video.videoId === videoId).length) {
+          return new PlaylistError('The playlist already contains this video', 200);
+        }
+        var position = playlist.videos.reduce((max,video) => {return Math.max(max, video.position)},0) + 1;
+        return db.knex(db.playlistVideos.table)
+        .insert({
+          playlistId: id,
+          videoId: videoId,
+          position: position,
+        });
+      }
+    })
+    .catch(function(err) {
+      return new PlaylistError(err.message, err.code);
+    });
+  };
+
+  playlists.removeVideoFromPlaylist = function(user, id, videoId) {
+    return db.knexnest( db.knex
+      .select(
+        `${playlists.table}.id as _id`,
+        `${db.playlistVideos.table}.videoId as _videos__videoId`,
+        `${db.playlistVideos.table}.position as _videos__position`,
+        `${playlists.table}.channelId as _channelId`,
+      )
+      .from(playlists.table)
+      .where(`${playlists.table}.id`, id)
+      .leftJoin(db.playlistVideos.table, `${playlists.table}.id`, `${db.playlistVideos.table}.playlistId`)
+      .leftJoin(db.channels.table, `${db.channels.table}.id`, `${playlists.table}.channelId`)
+      .modify(db.channels.authorizedManageSubquery, user)
+    )
+    .then(function(rows) {
+      if (rows.length) {
+        var playlist = rows[0];
+        if (!playlist.videos.filter(video => video.videoId === videoId).length) {
+          return new PlaylistError('The playlist does not contain this video', 200);
+        }
+        return db.knex(db.playlistVideos.table)
+        .where('playlistId', id)
+        .andWhere('videoId', videoId)
+        .del();
+      }
+    })
+    .catch(function(err) {
+      return new PlaylistError(err.message, err.code);
+    });
   };
 
   playlists.deletePlaylist = function(user, id) {
