2cdb5e4c8f1c795c2afe12ce700593e7caed2974
user <ramlevy@gmail.com>
2020-01-05 11:22:28 +0200

    videoLikes migration with Papi

--- packages/vod-api/migrations/20200104194800_update-videoLikes.js
+++ packages/vod-api/migrations/20200104194800_update-videoLikes.js
@@ -1,11 +1,13 @@
 exports.up = function(knex, Promise) {
-  return knex.schema
-    .raw('alter table "videoLikes" DROP CONSTRAINT "videoLikes_pkey"')
-    .raw('alter table "videoLikes" ADD PRIMARY KEY ("createdAt")');
+  return knex.schema.alterTable('videoLikes', function(t) {
+    t.dropPrimary();
+    t.primary(['videoId', 'channelId'])
+  })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.scehma
-    .raw('alter table "videoLikes" DROP CONSTRAINT "videoLikes_pkey"')
-    .raw('alter table "videoLikes" ADD PRIMARY KEY ("videoId")');
+  return knex.schema.alterTable('videoLikes', function(t) {
+    t.dropPrimary();
+    t.primary('videoId')
+  })
 };
