acb1f647b2ec65e8f0a17bb4cfe48b084e8e44c9
omer <Znet@ZnetPc.test.ram>
2020-05-20 10:24:09 +0300

    Refactored dockerfiles, workers and reverseproxy config

--- packages/vod-thumbnailgen/config/default.js
+++ packages/vod-thumbnailgen/config/default.js
@@ -1,8 +1,4 @@
 module.exports = {
-  server: {
-    port: process.env.PORT || '8083',
-  },
-  api: process.env.API_URL || 'http://localhost:9090/api',
   RabbitMQ: {
     port: process.env.RABBITMQ_PORT || "5672",
     host: process.env.RABBITMQ_HOSTNAME || "localhost",
